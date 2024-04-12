f395d905f0332926042556223e7482377c57b91d
lee na kyung <ni1722650@gmail.com>
2024-04-12 13:19:40 +0900

    📝 :: UserRepository

--- src/main/java/com/example/khtbe/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/khtbe/domain/user/domain/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.khtbe.domain.user.domain.repository;
 import com.example.khtbe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUserId(String userId);
+    boolean existsByUserId(String userId);
 }
