75a252742dcff2e45bba7d913add6761764efa8e
lee na kyung <ni1722650@gmail.com>
2024-04-16 08:31:56 +0900

    📑 :: 유저별 조회 기능

--- src/main/java/com/example/khtbe/domain/exercise/domain/repository/ExerciseRepository.java
+++ src/main/java/com/example/khtbe/domain/exercise/domain/repository/ExerciseRepository.java
@@ -1,7 +1,12 @@
 package com.example.khtbe.domain.exercise.domain.repository;
 
 import com.example.khtbe.domain.exercise.domain.Exercise;
+import com.example.khtbe.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
+    Page<Exercise> findExercisesByUser(User user, Pageable pageable);
+    Integer countByUser(User user);
 }
