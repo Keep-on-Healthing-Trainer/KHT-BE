7e334a40e732fa436bf2f8b98d7e7a27aa056dd8
lee na kyung <ni1722650@gmail.com>
2024-04-15 09:31:35 +0900

    📑 :: ExerciseRepository

---
+++ src/main/java/com/example/khtbe/domain/exercise/domain/repository/ExerciseRepository.java
@@ -0,0 +1,7 @@
+package com.example.khtbe.domain.exercise.domain.repository;
+
+import com.example.khtbe.domain.exercise.domain.Exercise;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
+}
