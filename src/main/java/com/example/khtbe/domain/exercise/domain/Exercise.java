c1c59f90caf3d3edb748de37973b3deae6cf96d2
lee na kyung <ni1722650@gmail.com>
2024-04-16 19:36:07 +0900

    ♻️ :: import문 풀기

--- src/main/java/com/example/khtbe/domain/exercise/domain/Exercise.java
+++ src/main/java/com/example/khtbe/domain/exercise/domain/Exercise.java
@@ -6,7 +6,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
