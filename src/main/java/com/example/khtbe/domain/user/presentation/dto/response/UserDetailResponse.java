0c4d47908dca10f77759043072e3d00440b75af0
lee na kyung <ni1722650@gmail.com>
2024-04-19 23:21:30 +0900

    📝 :: UserDetailResponse 수정

--- src/main/java/com/example/khtbe/domain/user/presentation/dto/response/UserDetailResponse.java
+++ src/main/java/com/example/khtbe/domain/user/presentation/dto/response/UserDetailResponse.java
@@ -3,18 +3,22 @@ package com.example.khtbe.domain.user.presentation.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class UserDetailResponse {
     private final String name;
     private final String userId;
     private final String profileImgeUrl;
     private final Integer totalCounts;
+    private final UUID id;
 
     @Builder
-    public UserDetailResponse (String name, String userId, String profileImgeUrl, Integer totalCounts) {
+    public UserDetailResponse (String name, String userId, String profileImgeUrl, Integer totalCounts, UUID id) {
         this.name = name;
         this.userId = userId;
         this.profileImgeUrl = profileImgeUrl;
         this.totalCounts = totalCounts;
+        this.id = id;
     }
 }
