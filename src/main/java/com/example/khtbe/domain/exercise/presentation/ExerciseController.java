ea441d5cdbd77c2223e3623e53964388f940a16c
lee na kyung <ni1722650@gmail.com>
2024-05-01 23:32:36 +0900

    주석처리

--- src/main/java/com/example/khtbe/domain/exercise/presentation/ExerciseController.java
+++ src/main/java/com/example/khtbe/domain/exercise/presentation/ExerciseController.java
@@ -4,10 +4,14 @@ import com.example.khtbe.domain.exercise.presentation.dto.request.ExerciseReques
 import com.example.khtbe.domain.exercise.presentation.dto.response.ExerciseGraphResponse;
 import com.example.khtbe.domain.exercise.presentation.dto.response.ExerciseResponse;
 import com.example.khtbe.domain.exercise.service.ExerciseService;
+import com.example.khtbe.domain.exercise.service.IpCheck;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @RestController
@@ -15,7 +19,6 @@ import java.util.UUID;
 @RequestMapping("/exercise")
 public class ExerciseController {
     private final ExerciseService exerciseService;
-
     @PostMapping("/{id}")
     public ExerciseResponse exercise(@PathVariable UUID id, @RequestBody ExerciseRequest request){
         return exerciseService.exercise(request, id);
@@ -25,4 +28,13 @@ public class ExerciseController {
     public ExerciseGraphResponse exerciseGraph(Pageable pageable) {
         return exerciseService.exerciseGraph(pageable);
     }
+
+//    @GetMapping(value = "/qr")
+//    public String loginQr(HttpServletRequest request, HttpServletResponse response, IpCheck ic) {
+//
+//        int qrResult = exerciseService.exerciseGetQr(request,response,ic);
+//
+//        if (qrResult == 1) return "/exercise/MainQrLogin";
+//        else return "/error";
+//    }
 }
