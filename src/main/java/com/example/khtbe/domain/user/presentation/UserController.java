165b1ec50eb30f387ab2f8727f35afe8e2cf4968
lee na kyung <ni1722650@gmail.com>
2024-05-13 17:28:41 +0900

    🐛 :: 러브버그

--- src/main/java/com/example/khtbe/domain/user/presentation/UserController.java
+++ src/main/java/com/example/khtbe/domain/user/presentation/UserController.java
@@ -13,15 +13,7 @@ import com.example.khtbe.domain.user.service.util.UserUtil;
 import com.example.khtbe.infra.aws.util.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -61,9 +53,9 @@ public class UserController {
         return userService.getUser();
     }
 
-    @PostMapping(value = "/modifyProfile/{userId}", consumes = {"multipart/form-data"})
+    @PostMapping(value = "/modifyProfile", consumes = {"multipart/form-data"})
     @ResponseStatus(HttpStatus.CREATED)
-    public void profileImage(@PathVariable String userId, @RequestPart(value = "image", required = false) MultipartFile file) {
+    public void profileImage(@RequestPart(value = "userId") String userId, @RequestPart(value = "image", required = false) MultipartFile file) {
         userUtil.upload(userId, file);
     }
 }
