8a365af108f2fd34740405d92334360cd10a7bd8
velyn666 <ni1722650@gmail.com>
2024-07-21 03:04:46 +0900

    📑 :: 가이드

---
+++ src/main/java/com/example/khtbe/domain/guide/domain/Guide.java
@@ -0,0 +1,57 @@
+package com.example.khtbe.domain.guide.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Guide {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String startPosture;
+
+    @Column(nullable = false)
+    private String exerciseMethod;
+
+    @Column(nullable = false)
+    private String warning;
+
+    @Column(nullable = false)
+    private String introduction;
+
+    @ElementCollection
+    private Set<String> tags;
+
+    private String path;
+
+    @Builder
+    public Guide(String title, String startPosture, String exerciseMethod, String warning, String introduction, String path){
+        this.title = title;
+        this.startPosture = startPosture;
+        this.warning = warning;
+        this.exerciseMethod = exerciseMethod;
+        this.introduction = introduction;
+        this.path = path;
+    }
+
+    public String updatePath(String path) {
+        this.path = path;
+        return path;
+    }
+}
