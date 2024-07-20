ea4c172799d80220c0df9bf039368700995f5f71
velyn666 <ni1722650@gmail.com>
2024-07-21 03:03:33 +0900

    📑 :: 운동 enum 추가

--- src/main/java/com/example/khtbe/domain/exercise/presentation/dto/request/ExerciseRequest.java
+++ src/main/java/com/example/khtbe/domain/exercise/presentation/dto/request/ExerciseRequest.java
@@ -1,13 +1,18 @@
 package com.example.khtbe.domain.exercise.presentation.dto.request;
 
+import com.example.khtbe.domain.exercise.domain.Exercise;
 import lombok.Getter;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class ExerciseRequest {
     @Max(500)
     @NotNull
-    Integer count;
+    private Integer count;
+
+    @NotBlank
+    private String type;
 }
--- src/main/java/com/example/khtbe/domain/exercise/presentation/dto/response/ExerciseResponse.java
+++ src/main/java/com/example/khtbe/domain/exercise/presentation/dto/response/ExerciseResponse.java
@@ -8,11 +8,13 @@ public class ExerciseResponse {
     private final Long id;
     private final Integer count;
     private final String exerciseDate;
+    private final String exType;
 
     @Builder
-    public ExerciseResponse(Long id, Integer count, String exerciseDate){
+    public ExerciseResponse(Long id, Integer count, String exerciseDate, String exType){
         this.id = id;
         this.count = count;
         this.exerciseDate = exerciseDate;
+        this.exType = exType;
     }
 }
