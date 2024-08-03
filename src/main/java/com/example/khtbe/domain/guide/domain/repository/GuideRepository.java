ce6512d8f6fc1247a4437b1f388f52e4e8a7a988
velyn666 <ni1722650@gmail.com>
2024-08-03 21:51:21 +0900

    📑 :: 가이드 레포지토리

--- src/main/java/com/example/khtbe/domain/guide/domain/repository/GuideRepository.java
+++ src/main/java/com/example/khtbe/domain/guide/domain/repository/GuideRepository.java
@@ -1,4 +1,8 @@
 package com.example.khtbe.domain.guide.domain.repository;
 
-public interface GuideRepository {
+import com.example.khtbe.domain.guide.domain.Guide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideRepository extends JpaRepository<Guide, Long> {
+
 }
