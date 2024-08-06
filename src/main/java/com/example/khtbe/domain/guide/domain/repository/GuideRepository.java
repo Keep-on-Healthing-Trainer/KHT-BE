65034f696f7c50aecb4fe6e9e561091e05788fe0
velyn666 <ni1722650@gmail.com>
2024-08-06 09:19:36 +0900

    📑 :: 레포지토리

--- src/main/java/com/example/khtbe/domain/guide/domain/repository/GuideRepository.java
+++ src/main/java/com/example/khtbe/domain/guide/domain/repository/GuideRepository.java
@@ -1,8 +1,14 @@
 package com.example.khtbe.domain.guide.domain.repository;
 
 import com.example.khtbe.domain.guide.domain.Guide;
+import com.example.khtbe.domain.guide.domain.tags.tagsEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GuideRepository extends JpaRepository<Guide, Long> {
-
+    @Query("SELECT g FROM Guide g JOIN g.tags t WHERE g.title LIKE %:keyword% OR t = :tagName")
+    List<Guide> findByTitleOrTag(@Param("keyword") String keyword, @Param("tagName") tagsEnum tagName);
 }
