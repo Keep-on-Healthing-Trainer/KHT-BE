2f6e8bbd9a15fc7ebf1103550705707e4d520663
velyn666 <ni1722650@gmail.com>
2024-08-13 22:10:54 +0900

    🗑 :: 필요없는 쿼리문 삭제

--- src/main/java/com/example/khtbe/domain/exercise/domain/repository/ExerciseRepository.java
+++ src/main/java/com/example/khtbe/domain/exercise/domain/repository/ExerciseRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
-    Page<Exercise> findExercisesByUser(User user, Pageable pageable);
     Page<Exercise> findExercisesByUserAndExType(User user, Exercise.ExerciseType exType, Pageable pageable);
     Integer countByUser(User user);
 }
