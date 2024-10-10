1bdb9fa2435ea7629c61d57822b70e95aaad32ae
O-damnpr <ni1722650@gmail.com>
2024-10-10 23:25:29 +0900

    에러코드문 변경

--- src/main/java/com/example/khtbe/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/khtbe/global/error/exception/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
 
     //UnAuthorised
     NOT_ACCESS_TOKEN(401, "access 토큰이 아닙니다."),
-    TOKEN_ERROR(401, "토큰이 잘못되지 않았는지 확인해주세요."),
+    TOKEN_ERROR(401, "토큰이 잘못되었습니다."),
     TOKEN_UNAUTHORIZED(401,"토큰이 승인 되지 않습니다."),
     JWT_INVALID(401, "토큰이 유효하지 않습니다."),
 
